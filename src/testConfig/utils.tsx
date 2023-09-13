18be9fff7d777044a28983ab5d5f160e8070cb61
Wenjun Qu <wqu1995@gmail.com>
2023-09-13 13:13:49 -0400

    added necessary packages and configuration for testing

---
+++ src/testConfig/utils.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+
+function customRender(ui:React.ReactElement, options = {}){
+    return render(ui, {
+        wrapper:({children}) => children,
+        ...options,
+    })
+}
+
+export * from '@testing-library/react'
+export{ default as userEvent} from '@testing-library/user-event'
+export { customRender as render }
---
+++ tests/App.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+import { screen } from '@testing-library/react';
+import App from "../src/App";
+import { render } from "../src/testConfig/utils";
+
+describe('App', ()=>{
+    it('renders the basic App component', () =>{
+        render(<App/>);
+        expect(screen.findByText("Vite + React")).toBeDefined();
+    })
+})
