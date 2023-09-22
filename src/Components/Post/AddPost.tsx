de1a6529581507be1182bf06b899eb3d35e90062
Austin Perrine <akperrine@gmail.com>
2023-09-22 10:11:27 -0700

    add basic handlesubmit and change to AddPost

--- src/Components/Post/AddPost.tsx
+++ src/Components/Post/AddPost.tsx
@@ -1,21 +1,34 @@
-import { Card, TextInput } from "@trussworks/react-uswds";
-import { ChangeEvent } from "react";
+import { Card, Form, TextInput } from "@trussworks/react-uswds";
+import React, { ChangeEvent, useState } from "react";
 
 function AddPost() {
+  const [postInput, setPostInput] = useState("");
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    e.preventDefault();
+    setPostInput(e.target.value);
   }
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(postInput);
+    setPostInput("");
+  }
+
   return (
     <Card>
       <div className="display-flex flex-row">
         <img alt="prof"></img>
-        <TextInput
-          id="nav-search"
-          name="nav-search"
-          type="text"
-          onChange={handleChange}
-          className="border-base-light"
-        />
+        <form onSubmit={handleSubmit}>
+          <TextInput
+            id="nav-search"
+            name="nav-search"
+            type="text"
+            value={postInput}
+            onChange={handleChange}
+            className="border-base-light"
+            placeholder="Start a Post"
+          />
+          <button>Post</button>
+        </form>
       </div>
     </Card>
   );
