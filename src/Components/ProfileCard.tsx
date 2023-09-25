e6b37093e9bcb81f954fe6bbe6d2fb01d2714d8b
Austin Perrine <akperrine@gmail.com>
2023-09-25 09:07:07 -0700

    make profile page take in a User state to populate data

--- src/Components/ProfileCard.tsx
+++ src/Components/ProfileCard.tsx
@@ -1,11 +1,20 @@
 import { Card } from "@trussworks/react-uswds";
+import { Link } from "react-router-dom";
+import Profile from "../pages/Profile";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../redux/slices/userSlice";
 
 function ProfileCard() {
+  const user = useSelector(selectCurrentUser);
+  console.log(user);
+
   return (
     <Card className="margin-2 margin-top-0">
       <div>
         <img alt="prof"></img>
-        <h3>First Last</h3>
+        <Link to={"/profile"} state={user}>
+          <h3>First Last</h3>
+        </Link>
         <p>Software Engineer | Java | Javascript | React | Spring</p>
       </div>
       <p>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,18 +2,23 @@ import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../redux/slices/userSlice";
 import { Grid, GridContainer } from "@trussworks/react-uswds";
 import PageWrapper from "../Components/UiComponents/PageWrapper";
+import { User } from "../utils/Types";
+import { useLocation } from "react-router-dom";
 
+// function Profile({ profileInfo }: ProfileProps) {
 function Profile() {
-  const user = useSelector(selectCurrentUser);
-
+  const location = useLocation();
+  const profileInfo = location.state as User;
+  // const user = useSelector(selectCurrentUser);
   return (
     <PageWrapper>
       <GridContainer>
         <Grid row>
           <Grid col={9} className="bg-blue">
             Profile Box
+            {" " + profileInfo.firstName}
           </Grid>
-          <Grid col={3}>Adds Box</Grid>
+          <Grid col={3}>Your ad here</Grid>
         </Grid>
       </GridContainer>
     </PageWrapper>
