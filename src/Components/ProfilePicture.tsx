38d4c3fde1629dafdc3550a84df92cd2e5f5c85e
Austin Perrine <akperrine@gmail.com>
2024-01-23 14:49:28 -0700

    add linked-in default profile picture

--- src/Components/ProfilePicture.tsx
+++ src/Components/ProfilePicture.tsx
@@ -15,7 +15,8 @@ function ProfilePicture({ imageUrl }: ProfilePictureProps) {
         src={
           imageUrl
             ? imageUrl
-            : "https://1.bp.blogspot.com/-pzkUACogq0E/X5OcHr5ZnSI/AAAAAAABb5Q/xb-j2PQXgu03_vypUL1XNOYv4bhpWEFgQCNcBGAsYHQ/s788/bird_mameruriha_inko_blue.png"
+            : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+          // "https://1.bp.blogspot.com/-pzkUACogq0E/X5OcHr5ZnSI/AAAAAAABb5Q/xb-j2PQXgu03_vypUL1XNOYv4bhpWEFgQCNcBGAsYHQ/s788/bird_mameruriha_inko_blue.png"
         }
         alt="pic"
       />
